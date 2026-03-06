200555495a2b06508d5440232f346e773711cad8
Shital Bagade <shitalbagade2005@gmail.com>
2026-03-06 16:28:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,39 +8,42 @@ let today = new Date().toISOString().split("T")[0];
 checkin.min = today;
 checkout.min = today;
 
-function calculateNights(){
+function calculateNights() {
+
 let checkinDate = new Date(checkin.value);
 let checkoutDate = new Date(checkout.value);
 
-if(checkin.value && checkout.value){
+if (checkin.value && checkout.value) {
+
 let timeDiff = checkoutDate - checkinDate;
 let days = timeDiff / (1000 * 3600 * 24);
 
-if(days > 0){
+if (days > 0) {
 nightsText.textContent = "Total Nights: " + days;
 errorMsg.textContent = "";
-}else{
+} 
+else {
 nightsText.textContent = "";
 }
+
 }
+
 }
 
 checkin.addEventListener("change", calculateNights);
 checkout.addEventListener("change", calculateNights);
 
 form.addEventListener("submit", function(e){
-e.preventDefault();
 
 let checkinDate = new Date(checkin.value);
 let checkoutDate = new Date(checkout.value);
 
-if(checkoutDate <= checkinDate){
+if (checkoutDate <= checkinDate) {
+e.preventDefault();
 errorMsg.textContent = "Check-out date must be after check-in date!";
 return;
 }
 
-errorMsg.textContent = "";
 alert("Your 5-Star Luxury Booking is Confirmed!");
-form.reset();
-nightsText.textContent = "";
+
 });
